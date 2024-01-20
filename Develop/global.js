592a35f55096d9eccb8fbd29e6030085d60543b7
Isaiah Apreku <isaiahapreku@isaiahs-air-2.mynetworksettings.com>
2024-01-20 01:48:01 -0500

    improvedata

--- Develop/global.js
+++ Develop/global.js
@@ -4,7 +4,14 @@ const getCategories = async (request, response) => {
     let errorMessage = `Error Getting Categories`;
     try {
         const categories = await Category.findAll({ includes: [Product] });
-        response.json(categories);
+        let modifiedCategories = categories.map(cat => {
+            return {
+                id: cat.id,
+                name: cat.name,
+                category_name: cat.name,
+            }
+        })
+        response.json(modifiedCategories);
     } catch (error) {
         console.log(errorMessage, error);
         response.status(500).json({ error: errorMessage });
@@ -15,7 +22,21 @@ const getProducts = async (request, response) => {
     let errorMessage = `Error Getting Products`;
     try {
         const products = await Product.findAll();
-        response.json(products);
+        const categories = await Category.findAll({ includes: [Product] });
+
+        let modifiedProducts = products.map(pr => {
+            return {
+                id: pr.id,
+                name: pr.name,
+                stock: pr.stock,
+                price: pr.price,
+                category_id: pr.category_id,
+                date: new Date().toLocaleString(),
+                category: categories.find(cat => cat.id == pr.category_id)?.name,
+            }
+        })
+
+        response.json(modifiedProducts);
     } catch (error) {
         console.log(errorMessage, error);
         response.status(500).json({ error: errorMessage });
@@ -36,8 +57,22 @@ const getTags = async (request, response) => {
 const getProductTags = async (request, response) => {
     let errorMessage = `Error Getting Product Tags`;
     try {
+        const tags = await Tag.findAll();
+        const products = await Product.findAll();
         const productTags = await ProductTag.findAll();
-        response.json(productTags);
+
+        let modifiedProductTags = productTags.map(prt => {
+            return {
+                id: prt.id,
+                tag_id: prt.tag_id,
+                product_id: prt.product_id,
+                date: new Date().toLocaleString(),
+                tag: tags.find(tg => tg.id == prt.tag_id)?.name,
+                product: products.find(pr => pr.id == prt.product_id)?.name,
+            }
+        })
+
+        response.json(modifiedProductTags);
     } catch (error) {
         console.log(errorMessage, error);
         response.status(500).json({ error: errorMessage });
